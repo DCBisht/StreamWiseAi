f9e4f987d10622601fd31e422266ecd951e970bf
Divya Chakshu Bisht <65598930+DCBisht@users.noreply.github.com>
2023-12-26 23:43:57 +0530

    Validation added to form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidData } from '../Utils/validate';
 
 const Login = () => {
+  const [isSignInForm,setIsSignInForm] = useState(true);
+  const email =useRef(null);
+  const password = useRef(null);
+  const name= useRef("h");
+  const [errorMessage, setErrorMessage] = useState();
+
+  const toggleSignUP= ()=>{
+    setIsSignInForm(!isSignInForm);
+  }
+  const handleButtonClick = ()=>{
+    if(isSignInForm)
+    setErrorMessage(checkValidData(email.current.value,password.current.value));
+    else
+    setErrorMessage(checkValidData(email.current.value,password.current.value, name.current.value));
+  }
   return (
     <div>
       <Header/>
@@ -9,11 +25,19 @@ const Login = () => {
       <img 
       src='https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/6b2842d1-2339-4f08-84f6-148e9fcbe01b/IN-en-20231218-popsignuptwoweeks-perspective_alpha_website_small.jpg'/>
       </div>
-      <form className=' bg-opacity-80 absolute rounded-lg p-14 bg-black w-4/12 my-36 mx-auto right-0 left-0 text-white'>
-        <h1 className=' font-bold text-3xl p-2 mb-6'> Sign In</h1>
-        <input type='text' placeholder='Email address' className='rounded-lg p-2 m-2 w-full'/>
-        <input type='password' placeholder='Password' className='rounded-lg p-2 m-2 w-full'/>
-        <button className='rounded-lg p-2 m-2 mb-10 bg-red-700 w-full'>Sign In</button>
+      <form 
+      onSubmit={(e)=> e.preventDefault()}
+      className=' bg-opacity-80 absolute rounded-lg p-14 bg-black w-4/12 my-36 mx-auto right-0 left-0 text-white'>
+        <h1 className=' font-bold text-3xl p-2 mb-6'> {isSignInForm?"Sign In":"Sign Up"} </h1>
+        {!isSignInForm && <input type='text' placeholder='Full Name' className='rounded-lg p-2 m-2 w-full bg-gray-800' ref={name}/>}
+        <input type='text' placeholder='Email address' className='rounded-lg p-2 m-2 w-full bg-gray-800' ref={email}/>
+       
+        <input type='password' placeholder='Password' className='rounded-lg p-2 m-2 w-full bg-gray-800' ref={password}/>
+        <p className='p-1 my-1 mx-2 text-red-500 font-bold'>{errorMessage}</p>
+        <button className='rounded-lg p-2 m-2 mx-3 bg-red-700 w-full' 
+        onClick={handleButtonClick}>{isSignInForm?"Sign In":"Sign Up"}</button>
+        
+        <p className='p-1 cursor-pointer' onClick={toggleSignUP}>{isSignInForm?"New to platform? Sign Up Now":"Already a user? Sign In"}</p>
       </form>
     </div>
   )
